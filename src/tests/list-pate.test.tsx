635b5268ece7201734d4b5a632e518b761660f5e
aaliapkin <aaliapkin@gmail.com>
2021-04-11 21:48:13 +0300

    try to mock fetch, which kinda sorta works

--- src/tests/list-pate.test.tsx
+++ src/tests/list-pate.test.tsx
@@ -3,36 +3,88 @@ import ListPage from "ts/components/pages/list-page"
 import { render, screen, fireEvent } from "@testing-library/react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { ITest } from "ts/model/test-model"
+import fetchMock from "jest-fetch-mock"
+
+fetchMock.enableMocks()
+
+const data = {
+  returnedData: [
+    {
+      id: 1,
+      name: "Prototype of the new map",
+      type: "CLASSIC",
+      status: "PAUSED",
+      siteId: 2,
+    },
+    {
+      id: 2,
+      name: "Dark theme test",
+      type: "MVT",
+      status: "DRAFT",
+      siteId: 3,
+    },
+  ],
+}
+
+const data1 = {
+  returnedData: [
+    {
+      id: 2,
+      url: "https://www.delivery.company.com",
+    },
+    {
+      id: 3,
+      url: "http://games.company.com",
+    },
+  ],
+}
+
+beforeEach(() => {
+  fetchMock.resetMocks()
+})
+// test-task-kameleoon.herokuapp.com
+beforeEach(() => {
+  fetchMock.mockIf(
+    /^https?:\/\/test-task-kameleoon\.herokuapp\.com\.*$/,
+    (req) => {
+      if (req.url.endsWith("/tests")) {
+        return Promise.resolve(JSON.stringify(data))
+      } else if (req.url.endsWith("/sites")) {
+        return Promise.resolve(JSON.stringify(data1))
+      } else {
+        return Promise.resolve({
+          status: 404,
+          body: "Not Found",
+        })
+      }
+    }
+  )
+})
 
 test("list-page should render data", () => {
-  const test: ITest = {
-    id: 1,
-    name: "Dark theme test",
-    type: "CLASSIC",
-    status: "PAUSED",
-    siteId: 2,
-    site: "delivery.company.com",
-    color: "rgb(194,194,255)",
-  }
-
   render(
     <Router>
       <ListPage />
     </Router>
   )
 
-  const text = screen.getByText("Dark theme test")
+  const text = screen.getByTestId("spinner-element")
+  expect(text).toBeInTheDocument()
+})
+
+test("list-page should render data", async () => {
+  render(
+    <Router>
+      <ListPage />
+    </Router>
+  )
+
+  const text = await screen.findByText("Prototype of the new map")
+  console.log(text)
+
   expect(text).toBeInTheDocument()
 
-  const text1 = screen.getByText("CLASSIC")
+  const text1 = await screen.findByText("Dark theme test")
+  console.log(text1)
   expect(text1).toBeInTheDocument()
-
-  const text2 = screen.getByText("paused")
-  expect(text2).toBeInTheDocument()
-
-  const text3 = screen.getByText("delivery.company.com")
-  expect(text3).toBeInTheDocument()
-
-  const text4 = screen.getByText("results")
-  expect(text4).toBeInTheDocument()
 })
